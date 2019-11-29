349aff4197c43fca0d28f0e11dadfbe4dde6ea11
Carlos Martinez <carlosm28@outlook.com>
2019-11-29 13:56:48 -0500

    Emails Finished

--- projectxapp/urls.py
+++ projectxapp/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
 
 
 
-        # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
         name='password_change_done'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
